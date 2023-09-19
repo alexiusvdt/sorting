2be6b0ea04fcde447918d345b21e471ed1d4e85f
alexiusvdt <alex.johnson293@gmail.com>
2023-09-19 14:42:27 -0700

    set up basic state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 import axios from "axios";
 import './App.css';
 import Navbar from './components/Navbar';
 import Visualizer from './components/Visualizer';
 
-function App() {
-
-  return (
-    <div className="App">
+class App extends Component {
+  state = { 
+    array: [],
+    arraySteps: [],
+    colorKey: [],
+    colorSteps: [],
+    currentStep: 0,
+    count: 0,
+    delay: 100,
+    algorithm: '',
+    timeouts: []
+  }
+  render() {
+    return(
+      <div className="App">
       <Navbar />
       <Visualizer />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
