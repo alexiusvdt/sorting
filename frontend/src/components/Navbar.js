130b6c8c29b104e17918ef8bab4b3d5e359043af
alexiusvdt <alex.johnson293@gmail.com>
2023-09-19 16:34:57 -0700

    redesign dropdown pickers to use state & better react functionality

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,22 +2,33 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function Navbar() {
- const [sortData, setSortData] = useState(null)
+  const [sortData, setSortData] = useState(null)
+  const [selectedSize, setSelectedSize] = useState(null)
+  const [selectedAlgo, setSelectedAlgo] = useState(null)
+  const [selectedSpeed, setSelectedSpeed] = useState(null)
 
- function getBubbleData() {
-   console.log('starting fetch')
-   axios({
-     method: "GET",
-     url:"/sort",
-     params: {
-       "func": "bubble",
-       size: 5
-     },
-   })
-   .then((response) => {
+ /**
+ * @function
+ * calls the api & returns an object & stashes into state
+ * 0 will be the original randomized array
+ * 1 will contain subarrays with each step of the process
+ * ~currently hardcoded~
+ */
+
+  function getBubbleData() {
+    console.log('starting fetch')
+    axios({
+      method: "GET",
+      url:"/sort",
+      params: {
+        "func": "bubble",
+        size: 5
+      },
+    })
+    .then((response) => {
     console.log(response)
-     const res = response.data
-     setSortData(({res}))
+    const res = response.data
+    setSortData(({res}))
    }).catch((error) => {
      if (error.response) {
        console.log(error.response)
@@ -40,28 +51,40 @@ function submitData() {
     <div className="navbar" id="navbar">
         <p>request a sort: </p><button onClick={getBubbleData}>Dummy Request</button>
         <div className="form">
-            <label>Size: </label>
-            <select id="arrSize" name="size">
-                <option value="10">10</option>
-                <option value="25">25</option>
-                <option value="50" selected>50</option>
-                <option value="100">100</option>
-            </select>
-            <label>Algorithm: </label>
-            <select id="algorithm" name="algorithm">
-                <option value="bubble">Bubble Sort</option>
-                <option value="merge">Merge Sort</option>
-                <option value="heap">Heap Sort</option>
-            </select>
-            <label>Speed: </label>
-            <select id="speed" name="speed">
-                <option value="half">0.5x</option>
-                <option value="standard" selected>1x</option>
-                <option value="fast">2x</option>
-            </select>
-
-            <button id="start" onClick = {submitData()}>Show me!</button>
-            {/* <button id="print" onClick = {showSortData()}>show me the data</button> */}
+        <label>Array Size: </label>
+        <select
+          value={selectedSize}
+          defaultValue={"10"}
+          onChange={(e) => setSelectedSize(e.target.value)}
+        >
+          {/* <option value={"placeholder"}>Choose one...</option> */}
+          <option value={"10"}>10</option>
+          <option value={"25"}>25</option>
+          <option value={"50"}>50</option>
+          <option value={"100"}>100</option>
+        </select>
+        <label>Algorithm: </label>
+        <select
+          value={selectedAlgo}
+          defaultValue={"bubble"}
+          onChange={(e) => setSelectedAlgo(e.target.value)}
+        >
+          <option value={"bubble"}>Bubble Sort</option>
+          <option value={"merge"}>Merge Sort</option>
+          <option value={"tbd"}>Heap Sort</option>
+        </select>
+        <label>Speed: </label>
+        <select
+          value={selectedSpeed}
+          defaultValue={"std"}
+          onChange={(e) => setSelectedSpeed(e.target.value)}
+        >
+          <option value={"half"}>0.5x</option>
+          <option value={"std"}>1x</option>
+          <option value={"fast"}>2x</option>
+          <option value={"sanic"}>100x</option>
+        </select>
+        <button id="start" onClick = {submitData()}>Show me!</button>
         </div>
     </div>
   );
